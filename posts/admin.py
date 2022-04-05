56527cb65fb5fcb0f2a80f52df09a4be36dd89cd
Merlin <m333rl1n@gmail.com>
2022-04-05 15:15:27 +0430

    Post model added

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'date', 'published')
+    list_filter = ('published', )
+    search_fields = ('title', 'text', )
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey('accounts.MyUser', on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    text =  models.TextField(max_length=20_000)
+    date = models.DateField(auto_now=True)
+    read_time = models.IntegerField()
+    slug = models.SlugField(unique=True)
+    published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.title}'
+    
+
