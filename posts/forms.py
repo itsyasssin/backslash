d3775db020876c08469e4b06510f036240c5b7d3
Merlin <m333rl1n@gmail.com>
2022-05-31 15:03:44 +0430

    PostForm added

--- posts/forms.py
+++ posts/forms.py
@@ -1,2 +1,9 @@
-from django.forms import ModelForm
+from django import forms
+from posts.models import Post
 
+class PostForm(forms.ModelForm):
+    
+    class Meta:
+        model = Post
+        fields = ('user','title','text','slug','published','tags')
+    
