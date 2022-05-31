53509008b448b948d79793acf7c23e1f217b0ed0
Merlin <m333rl1n@gmail.com>
2022-05-31 15:07:47 +0430

    send title to template

--- accounts/views.py
+++ accounts/views.py
@@ -5,8 +5,8 @@ from django.shortcuts import redirect, render
 def auth(request):
     if request.user.is_authenticated:
         return redirect('/')
-        
-    return render(request, 'index.html')
+    title  = request.META['PATH_INFO'].split("/")[2].replace('-',' ').capitalize()
+    return render(request, 'index.html', {'title': title})
 
 
 def log_out(request):
