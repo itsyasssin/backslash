8f16a393094fe7ca57dea3b2cbe6f287c56bd2a6
Merlin <m333rl1n@gmail.com>
2022-05-31 15:04:48 +0430

    simple path to route

--- posts/urls.py
+++ posts/urls.py
@@ -5,5 +5,14 @@ from .views import *
 app_name = 'posts'
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('@<username>/<slug>', post_detail, name='post_detail'),
+    path('@<username>', people_view, name='people_view'),
+    path('write/<id>', edit_post, name='edit_post'),
+    path('write', write, name='write'),
+    path('t/<name>', tag_view, name='tags_view'),
+    path('search', search, name='search'),
+    path('bookmarks', bookmarks, name='bookmarks'),
+    path('me/settings', settings, name='settings'),
+    path('me', me, name='me'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,38 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html',{'title': 'Home'})
+
+def post_detail(request,username, slug): 
+    return render(request, 'index.html',{'title': ''})
+
+def people_view(request,username):
+    return render(request, 'index.html',{'title': ''})
+
+def tag_view(request,name):
+    return render(request, 'index.html',{'title': ''})
+
+def search(request):
+    return render(request, 'index.html',{'title': 'Search'})
+
+@login_required(login_url='/accounts/sign-in')
+def edit_post(request, id):
+    return render(request, 'index.html',{'title': 'Edit'})
+
+@login_required(login_url='/accounts/sign-in')
+def write(request):
+    return render(request, 'index.html',{'title': 'Write'})
+
+@login_required(login_url='/accounts/sign-in')
+def bookmarks(request):
+    return render(request, 'index.html',{'title': 'Bookmarks'})
+
+@login_required(login_url='/accounts/sign-in')
+def settings(request):
+    return render(request, 'index.html',{'title': 'Settings'})
+
+@login_required(login_url='/accounts/sign-in')
+def me(request):
+    return render(request, 'index.html',{'title': 'Me'})
